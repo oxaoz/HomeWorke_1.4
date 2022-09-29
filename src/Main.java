8fd7526ca23aa12b06cf559b66af6238018d1d8d
ozerova <ogozer@ya.ru>
2022-09-29 13:27:30 +0700

    Задание 1.4_1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Задача N1
-        System.out.println("Задача N1");
+        // Зададание N1
+        // Задача № 1
+        System.out.println("Задание 1, Задача №1");
         int age = 19;
         if (age >= 18) {
             System.out.println("Поздравляю, дружище, детсво кончилось!");
@@ -13,10 +14,10 @@ public class Main {
 
         // Задача N2
         // мой вариант
-        System.out.println("Задача N2");
+        System.out.println("Задание 1, Задача №2");
         int childAge = 8;
         if (childAge >= 7) {
-         System.out.println("Ребенок ходит в школу");
+            System.out.println("Ребенок ходит в школу");
         }
         if (childAge < 7) {
             System.out.println("Ребенку рано в школу");
@@ -29,7 +30,7 @@ public class Main {
             System.out.println("Надеюсь, ты не расстроился... Ты сможешь поступить в универ после окончания школы");
         }
         int peopleAge = 25;
-        if (peopleAge >=24) {
+        if (peopleAge >= 24) {
             System.out.println("Началась взрослая жизнь, пора искать работу");
         }
         if (peopleAge < 24) {
@@ -37,7 +38,7 @@ public class Main {
         }
 
         // Вариант из проверочного видео
-        if (personAge >= 7 && personAge < 18){
+        if (personAge >= 7 && personAge < 18) {
             System.out.println("Ребенок ходит в школу");
         }
         if (personAge >= 18 && personAge < 24) {
@@ -48,7 +49,7 @@ public class Main {
         }
 
         // Задача N3
-        System.out.println("Задача N3");
+        System.out.println("Задание 1, Задача №3");
         int wagonCapacity = 102;
         int seatPlace = 60;
         int standingPlace = wagonCapacity - seatPlace; // стоячих мест
@@ -56,17 +57,61 @@ public class Main {
         int seatUsed = 60;
         int standingUsed = 35;
 
-        if (seatUsed<seatPlace) {
+        if (seatUsed < seatPlace) {
             System.out.println("Свободно " + (seatPlace - seatUsed) + " Сидячих мест ");
         }
-        if (standingUsed<standingPlace){
-                System.out.println("Свободно " + (standingPlace - standingUsed) + " Стоячих мест ");
+        if (standingUsed < standingPlace) {
+            System.out.println("Свободно " + (standingPlace - standingUsed) + " Стоячих мест ");
         }
-        if (seatPlace==seatUsed){
+        if (seatPlace == seatUsed) {
             System.out.println("Сидячих мест нет");
         }
-        if (standingPlace==standingUsed){
+        if (standingPlace == standingUsed) {
             System.out.println("Стоячих мест нет");
         }
+        // Задание N2
+        // Задача №1
+        System.out.println("Задание 2, Задача №1");
+        int personeAge = 35;
+        if (personeAge >= 18) {
+            System.out.println("Поздравляю, дружище, детсво кончилось!");
+        } else {
+            System.out.println("Поздравляю, друг, Ты ещЁ мал! Тебе повезло!");
+        }
+
+        // Задача N2
+        System.out.println("Задание 2, Задача №2");
+        if (personeAge >= 7 && personeAge < 18) {
+            System.out.println("Ребенок ходит в школу");
+        } else if (personeAge >= 18 && personeAge < 24) {
+            System.out.println("Студенческие годы");
+        } else if (personeAge >= 24) {
+            System.out.println("Универ за плечами, трудовые будни");
+        }
+
+        // Задача N3
+        System.out.println("Задание 2, Задача №3");
+        int salunCapacity = 102;
+        int seatCount = 60;
+        int standingCount = salunCapacity - seatCount; // стоячих мест
+
+        int seatUse = 60;
+        int standingUse = 42;
+
+        if (seatUse < seatCount) {
+            System.out.println("Свободно " + (seatCount - seatUse) + " Сидячих мест ");
+        } else {
+            System.out.println("Сидячих мест нет");
+        }
+        if (standingUse < standingCount) {
+            System.out.println("Свободно " + (standingCount - standingUse) + " Стоячих мест ");
+        } else {
+            System.out.println("Стоячих мест нет");
+        }
+
+        // Задание 3
+        // Задача №1
+        System.out.println("Задание 3, Задача №3");
+
     }
 }
